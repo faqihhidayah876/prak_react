a163241060cb68bee85f9f10e57f53f8db4c8311
Faqih <faqihhidayah05@gmail.com>
2026-04-07 15:27:42 +0700

    progress pertemuan 4 (search & filter)

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
 import './tailwind.css';
 import FrameworkList from "./components/FrameworkList";
+import FrameworkListSearchFilter from "./components/FramewrokListSeacrhFilter";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-            <FrameworkList/>
+            <FrameworkListSearchFilter/>
         </div>
     )
