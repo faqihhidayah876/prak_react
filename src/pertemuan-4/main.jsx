67a8cd1dc4bcdd98c3856b41749d74882f14309e
Faqih <faqihhidayah05@gmail.com>
2026-04-07 16:32:43 +0700

    pertemuan 4

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import './tailwind.css';
 import FrameworkList from "./components/FrameworkList";
 import FrameworkListSearchFilter from "./components/FramewrokListSeacrhFilter";
+import ResponsiveText from "./ResponsiveDesign";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-            <FrameworkListSearchFilter/>
+            {/* <FrameworkListSearchFilter/> */}
+            <ResponsiveText/>
         </div>
     )
